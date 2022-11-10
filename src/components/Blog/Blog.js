b28aec8add004b4fce8be8f6cfe2f29972625208
SkPipul <skjabed252@gmail.com>
2022-11-10 20:44:14 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,34 +5,34 @@ const Blog = () => {
         <div className='mx-[120px]'>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-3/4 my-3">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                Difference between SQL and NoSQL
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-3/4 mb-3">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                What is JWT, and how does it work?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-3/4 mb-3">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                What is the difference between javascript and NodeJS?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-3/4 mb-8">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                How does NodeJS handle multiple requests at the same time?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
                 </div>
             </div>
         </div>
